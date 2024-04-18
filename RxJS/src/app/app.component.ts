0d09e620fc716b4df14343d8ce3172f8a534d1c5
lyyang2 <lydia.yang@amadeus.com>
2024-04-18 11:51:10 +0100

    Create observable

--- RxJS/src/app/app.component.ts
+++ RxJS/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateComponentComponent } from './create-component/create-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreateComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'RxJS';
--- RxJS/src/app/create-component/create-component.component.ts
+++ RxJS/src/app/create-component/create-component.component.ts
@@ -1,22 +1,57 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { of } from 'rxjs';
+import { Subscription, from, fromEvent, of } from 'rxjs';
 
 @Component({
   selector: 'app-create-component',
   standalone: true,
   imports: [],
   templateUrl: './create-component.component.html',
-  styleUrl: './create-component.component.css'
+  styleUrl: './create-component.component.css',
 })
 export class CreateComponentComponent implements OnInit, OnDestroy {
+  subOf!: Subscription;
+  subOfArray!: Subscription;
+  subFrom!: Subscription;
+  subEventClick!: Subscription;
+  subEventKey!: Subscription;
+
   ngOnInit(): void {
-
     // Create using of
-    of(2, 4, 6, 8);
+    this.subOf = of(2, 4, 6, 8).subscribe((item) =>
+      console.log('Value from of: ', item)
+    );
 
+    // Create using of array
+    this.subOfArray = of([2, 4, 6, 8]).subscribe((item) =>
+      console.log('Value from of array: ', item)
+    );
+
+    // create using from
+    this.subFrom = from([20, 15, 10, 5]).subscribe({
+      next: (item) => console.log('From item: ', item),
+      error: (err) => console.log('From error: ', err),
+      complete: () => console.log('From complete'),
+    });
+
+    // create using fromEvent
+    this.subEventClick = fromEvent(document, 'click').subscribe({
+      next: (ev) => console.log('Click event: ', ev.target),
+      error: (err) => console.log('Error occurred', err),
+      complete: () => console.log('No more events'),
+    });
+
+    const keys: string[] = [];
+    this.subEventKey = fromEvent(document, 'keydown').subscribe((ev) => {
+      keys.push((ev as KeyboardEvent).key);
+      console.log('Key event: ', keys);
+    });
   }
 
   ngOnDestroy(): void {
-
+    this.subOf.unsubscribe();
+    this.subOfArray.unsubscribe();
+    this.subFrom.unsubscribe();
+    this.subEventClick.unsubscribe();
+    this.subEventKey.unsubscribe();
   }
 }
