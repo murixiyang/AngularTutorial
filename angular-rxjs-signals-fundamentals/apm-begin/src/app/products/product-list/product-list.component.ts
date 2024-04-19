b767bd0f8c550fdcbe71a0cd09c5fb24c38830ff
lyyang2 <lydia.yang@amadeus.com>
2024-04-19 14:14:40 +0100

    Retrieve products

--- angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product-list/product-list.component.ts
+++ angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product-list/product-list.component.ts
@@ -1,25 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
-    selector: 'pm-product-list',
-    templateUrl: './product-list.component.html',
-    standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, NgClass, ProductDetailComponent],
 })
-export class ProductListComponent {
-  // Just enough here for the template to compile
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Products';
   errorMessage = '';
+
+  sub!: Subscription;
 
   // Products
   products: Product[] = [];
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
+
+  constructor(private productSvc: ProductService) {}
+
+  ngOnInit(): void {
+    this.sub = this.productSvc
+      .getProducts()
+      .pipe(tap(() => console.log('In product-list init pipeline')))
+      .subscribe((products) => (this.products = products));
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
--- angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product.service.ts
+++ angular-rxjs-signals-fundamentals/apm-begin/src/app/products/product.service.ts
@@ -1,10 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  // Just enough here for the code to compile
   private productsUrl = 'api/products';
 
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http
+      .get<Product[]>(this.productsUrl)
+      .pipe(tap(() => console.log('In http pipe')));
+  }
 }
